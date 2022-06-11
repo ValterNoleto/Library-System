70f77630100cd29a2d22f22986eb68b9cb15d948
João Rodrigues De Melo Neto <j_neto_rodrigues@hotmail.com>
2022-06-10 23:45:57 -0300

    Basic example model

--- model/tests.py
+++ model/tests.py
@@ -3,3 +3,21 @@
 #devolver um livro nao emprestado
 #limitar emprestimo de no maximo 3 livros
 #solicitar emprestimo com funcionario invalido
+
+import unittest
+from main import *
+
+class Tests(unittest.TestCase):
+    def test_VerificarLivroJaEmprestado(self):
+        livro = Livro(1, "Como programar em python pulando paraquedas")
+        biblioteca = Biblioteca()
+        funcionario_novo = Funcionario(999999999999, "Dona Neusa")
+        biblioteca.cadastrarFuncionario(funcionario_novo)
+
+        usuario_qualquer = Usuario("Irvani", "Mora Na caixa prego")
+        resposta = biblioteca.realizarEmprestimo(funcionario_novo, livro, usuario_qualquer)
+
+        self.assertEqual("Emprestimo realizado com sucesso", resposta)
+
+        nova_resposta = biblioteca.realizarEmprestimo(funcionario_novo, livro, usuario_qualquer)
+        self.assertNotEquals("Emprestimo realizado com sucesso", nova_resposta)
